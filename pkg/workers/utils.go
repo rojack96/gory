e88e3293e55b1b19fc4beffaba27e8828e80a8b1
rojack96 <rojack96@protonmail.com>
2025-06-29 11:10:56 +0200

    added modify flag and resolved a bug when use -n and -s together

--- pkg/workers/utils.go
+++ pkg/workers/utils.go
@@ -3,10 +3,11 @@ package workers
 import "flag"
 
 type FlagReaderStruct struct {
-	Number int
-	Search string
-	//Unsafe bool
+	Modify   bool
+	Number   int
 	ReadOnly bool
+	Search   string
+	//Unsafe bool
 }
 
 // FlagReader reads command line flags.
@@ -14,9 +15,11 @@ func FlagReader() FlagReaderStruct {
 	var (
 		number   *int    = flag.Int("number", 10, "Number of commands to show")
 		n        *int    = flag.Int("n", 10, "Number of commands to show")
+		readOnly *bool   = flag.Bool("read-only", false, "Not execute commands, just show them")
+		modify   *bool   = flag.Bool("modify", false, "Allow the user to modify the selected command")
+		m        *bool   = flag.Bool("m", false, "Allow the user to modify the selected command")
 		search   *string = flag.String("search", "", "Search for a specific command in the history")
 		s        *string = flag.String("s", "", "Search for a specific command in the history")
-		readOnly *bool   = flag.Bool("read-only", false, "Not execute commands, just show them")
 		//unsafe *bool   = flag.Bool("unsafe", false, "Run unsafe commands")
 	)
 
@@ -32,10 +35,16 @@ func FlagReader() FlagReaderStruct {
 		finalSearch = *s
 	}
 
+	finalModify := *modify
+	if *m {
+		finalModify = *m
+	}
+
 	return FlagReaderStruct{
+		Modify:   finalModify,
 		Number:   finalNumber,
-		Search:   finalSearch,
 		ReadOnly: *readOnly,
+		Search:   finalSearch,
 		//Unsafe: *unsafe,
 	}
 
